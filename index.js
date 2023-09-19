03cd3685bfe0f108a769f8ff36001cd949a7d34a
AhmedNazeer <iamahmad9311@gmail.com>
2023-09-19 22:41:56 +0500

    main

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
 });
 
-app.listen(5000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Listening on port 5000!");
 });
