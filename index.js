5f293e91d8256ddd4960d95d10b013a93ab7a71f
AhmedNazeer <iamahmad9311@gmail.com>
2023-09-20 00:05:20 +0500

    main

--- index.js
+++ index.js
@@ -6,25 +6,24 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
 const stripe = require("stripe")(process.env.STRIPE_KEY);
-const mongoose = require('mongoose');
-
 const app = module.exports = express();
 
+const mongoose = require('mongoose');
+
 const connectDb = async () => {
-  try {       
+  try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected successfully to MongoDB");
+    console.log("Connected successfully");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.log(err)
   }
 }
 
-connectDb();
+connectDb()
+
 
 app.use(helmet());
 app.use(cookieParser());
-
-app.set('trust proxy', 1);
 
 var sess = {
   secret: process.env.SESSION_SECRET,
@@ -36,6 +35,9 @@ var sess = {
   saveUninitialized: true,
 }
 
+app.set('trust proxy', 1);
+sess.cookie.secure = true
+
 // if (app.get('env') === 'production') {
 //   app.set('trust proxy', 1);
 //   sess.cookie.secure = true;
@@ -44,40 +46,31 @@ var sess = {
 app.use(session(sess))
 
 // if (result.error) throw result.error;
+require(__dirname + '/orders.js');
+require(__dirname + '/admin.js');
 
 app.use(bodyParser.json());
 
 app.get('/product-info/:id', function (req, res) {
   stripe.skus.list({ product: req.params.id },
     function (err, product) {
-      if (err) {
-        console.error("Stripe SKU list error:", err);
-        return res.status(500).send(err);
-      }
-      res.json(product);
+      err ? res.status(500).send(err) : res.json(product);
     });
 });
 
 app.get('/product-info/', function (req, res) {
   stripe.skus.list(
     function (err, skus) {
-      if (err) {
-        console.error("Stripe SKU list error:", err);
-        return res.status(500).send(err);
-      }
-      res.json(skus.data);
+      err ? res.status(500).send(err) : res.json(skus.data);
     });
 });
 
-app.use(express.static(path.join(__dirname, '.', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
-  return res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, function () {
-  console.log("Listening on port", PORT);
+app.listen(5000, function () {
+  console.log("Listening on port 5000!");
 });
